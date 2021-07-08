0bde804b947be2c1c1ac11d7bda2e5711f444c9a
LORDDIOS <77187471+LORDDIOS@users.noreply.github.com>
2021-07-08 05:03:10 +0300

    Update stickyTemp.js

--- models/stickyTemp.js
+++ models/stickyTemp.js
@@ -1,29 +1,17 @@
-models/stickyTemp
-
 const mongoose = require("mongoose");
 
 const reqString = {
-
-  
+  type: String,
   required: true
-
 };
 
 const StickyTempSchema = new mongoose.Schema({
-
   lastStickyID: reqString,
-
   ChannelID: reqString,
-
   GuildID: reqString
-
 });
 
 const MessageModel = (module.exports = mongoose.model(
-
   "StickyTemp",
-
   StickyTempSchema
-
 ));
-
